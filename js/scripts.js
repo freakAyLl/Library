2d84256efe6ce381469696e51db1f5c7c61a1fea
FreakAyLl <alitofigueras5@gmail.com>
2021-05-29 01:16:24 -0300

    added a method to create cards for each book

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,37 @@
 const sendBtn= document.getElementById('sendButton')
 let bookArr= []
 //functions
-function Book([title, author, pages, read]){
+function Book(title, author, pages, read){
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
     this.info = function(){
-        return `${title}, ${author}, ${pages} pages, ${read}`
+        return `tittle: ${element.title}, author: ${element.author}, pages: ${element.pages}, reader: ${element.read}`
     }
 }
-
+let actual= 0
 function addBookToLibrary() {
     let newBook = document.querySelectorAll('[data-input]')
-    let actualArr= []
-    for(let i=0;i<4;i++){actualArr.push(newBook[i].value)}
-    let actual = new Book(actualArr)
+    actual = new Book(newBook[0].value,newBook[1].value,newBook[2].value,newBook[3].value)
     bookArr.push(actual)
-    //console.log(actual)
-    //console.log(bookArr)
     for(let i=0;i<4;i++){newBook[i].value=''}
+    displayBooks()
+    //console.log(actual)
+    console.log(bookArr)
+    actual++
+}
+function displayBooks(){
+    let gridContainer= document.getElementById('grid-container')
+    bookArr.forEach(element=>{
+        console.log(element.title)
+        if (document.getElementById(element.title)!=null){return}
+        element.title=document.createElement('div')
+        element.title.id= element.title
+        element.title.classList.add('card')
+        //newDiv.innerHTML=info()
+        gridContainer.appendChild(element.title)
+    })
 }
 //listeners
 sendBtn.addEventListener('click', function(){addBookToLibrary()})
