727f569af3a63b362f42ebaf8664340b6f8d034b
FreakAyLl <alitofigueras5@gmail.com>
2021-06-07 19:34:20 -0300

    JavaScript code restructured from 0

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,22 @@
 //variables
-const sendBtn= document.getElementById('sendButton')
-let bookArr= []
+const submitBtn= document.getElementById('submitButton')
+let myLybray= []
+let bookInfo= document.querySelectorAll('[data-input]')
 //functions
-function Book(title, author, pages, read){
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = function(){
-        return `tittle: ${element.title}, author: ${element.author}, pages: ${element.pages}, reader: ${element.read}`
-    }
+class Book{
+   constructor(name, author, status){
+    this.name=name;
+    this.author=author;
+    this.status=status;
+   }
 }
-let actual= 0
-function addBookToLibrary() {
-    let newBook = document.querySelectorAll('[data-input]')
-    actual = new Book(newBook[0].value,newBook[1].value,newBook[2].value,newBook[3].value)
-    bookArr.push(actual)
-    for(let i=0;i<4;i++){newBook[i].value=''}
-    displayBooks()
-    //console.log(actual)
-    console.log(bookArr)
-    actual++
+let addBook =()=>{
+    let newBook = new Book(bookInfo[0].value, bookInfo[1].value, bookInfo[2].value)
+    myLybray.push(newBook)
+    console.log(myLybray)
+    clearInput()
 }
-function displayBooks(){
-    let gridContainer= document.getElementById('grid-container')
-    bookArr.forEach(element=>{
-        console.log(element.title)
-        if (document.getElementById(element.title)!=null){return}
-        element.title=document.createElement('div')
-        element.title.id= element.title
-        element.title.classList.add('card')
-        //newDiv.innerHTML=info()
-        gridContainer.appendChild(element.title)
-    })
-}
+let clearInput=()=>{bookInfo.forEach(function(e){e.value=''})}
+
 //listeners
-sendBtn.addEventListener('click', function(){addBookToLibrary()})
+submitBtn.addEventListener('click',()=>{ addBook()})
